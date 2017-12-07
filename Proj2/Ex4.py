096b5db5a40a44baa03748369c80259e7d4c0ce9
SofiaAparicio <sofia.aparicio@tecnico.ulisboa.pt>
2017-12-07 04:59:48 +0000

    Ex4 doneeeeeeee <3

--- Proj2/Ex4.py
+++ Proj2/Ex4.py
@@ -9,15 +9,101 @@
 
 ################################################
 #                   imports                    #
+#pip install django							   #
 ################################################
-from Ex3 import TfidfTransformer_2, TfidfVectorizer_2, Graph, Vertex, Edge
-# import xml.etree.ElementTree
+from Ex1 import *
+import feedparser
+import webbrowser
+from yattag import Doc
+################################################
+#                 constants                    #
+################################################
+websites = ['http://rss.nytimes.com/services/xml/rss/nyt/World.xml','http://rss.cnn.com/rss/edition_world.rss','http://feeds.washingtonpost.com/rss/world','http://www.latimes.com/world/rss2.0.xml']
+
+template = """
+<html>
+<head>
+<title>Template {{ title }}</title>
+</head>
+<body>
+Body with {{ mystring }}.
+</body>
+</html>
+"""
 ################################################
 #                 functions                    #
 ################################################
 
-def exercise_4_main():
+def striphtml(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
 
+def getTaggs(tags):
+	listtags = []
+	for tag in tags:
+		listtags.append(tag['term'])
+	return listtags
+
+def getSiteData(site):
+	dicSite, tags, summary,published = {}, [], '', ''
+	feed = feedparser.parse(site)
+	for entrie in feed['entries']:
+		if len(entrie['title']) > 0:
+			if 'summary' in entrie.keys() :
+				summary = striphtml(entrie['summary'])
+			if 'tags' in entrie.keys() :
+				tags = getTaggs(entrie['tags'])
+			if 'published' in entrie.keys() :
+				published = entrie['published']
+			dicSite.update({striphtml(entrie['title']) : [summary,tags,entrie['links'][0]['href'],published  ]})
+
+	return dicSite
+
+def collectAllDataWeb():
+	globalMatrix = {}
+	for site in websites:
+		globalMatrix.update(getSiteData(site))
+	print(globalMatrix)
+
+	return globalMatrix
+
+def generateHTML(sentences, data):
+	f = open('helloworld.html','w')
+	urlPath = 'file://' + os.path.dirname(os.path.abspath('helloworld.html')) + '/helloworld.html'
+	doc, tag, text = Doc().tagtext()
+
+	with tag('h1'):
+	    text('World News!')
+	for sent in sentences:
+		with tag('h1'):
+			with tag('a', href=data[sent][2]):
+				text(sent)
+				doc.stag('br')
+		with tag('p'):
+			text(data[sent][3])
+		if len(data[sent][0]) > 0:
+			with tag('p'):
+				text('Summary of the article: ' + data[sent][0])
+		if len(data[sent][1]) > 0:
+			with tag('p'):
+				text('Tags: ')
+				for t in  data[sent][1]:
+					text(t)
+
+	f.write(doc.getvalue())
+
+	f.close()
+	webbrowser.open(urlPath, new=1, autoraise=True)
+
+def getGlobalSummary(matrix):
+	graph = Graph(matrix)
+	sentSum = graph.getSummary(SENT_SUM)
+	return sentSum
+
+def exercise_4_main():
+	data = collectAllDataWeb()
+	sumary = getGlobalSummary(data.keys())
+	generateHTML(sumary, data)
 
 ################################################
 #                     run                      #
